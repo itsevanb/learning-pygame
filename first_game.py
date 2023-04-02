3632f662ae69799953d50ca6a04ae110fc2ae321
Evan <116227653+itsevanb@users.noreply.github.com>
2023-04-02 18:53:43 -0500

    Update first_game.py
    got a collision screen, asks user if would like to restart game

--- first_game.py
+++ first_game.py
@@ -6,6 +6,7 @@ def display_score():
     score_surf = font.render(f'Score {current_time}', False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf, score_rect)
+    return current_time
     
 
 pygame.init()   # Initialize pygame
@@ -17,6 +18,7 @@ clock = pygame.time.Clock() # Create a clock object
 font = pygame.font.Font('font/Blox2.ttf', 50)   # Create a font object
 game_active = True
 start_time = 0 # Set the start time of the game
+score = 0
 
 sky_surface = pygame.image.load('graphics/sky.png').convert() # Load the image and convert it to a surface
 sky_surface = pygame.transform.scale(sky_surface, (screen_width, screen_height))
@@ -37,6 +39,15 @@ snail_rect = snail_surface.get_rect(bottomright = (700, screen_height - ground_h
 player_surface = pygame.image.load('graphics/player_walk/p2_walk01.png').convert_alpha() # Load the image and convert it to a surface
 player_rect = player_surface.get_rect(midbottom = (80, screen_height - ground_height))
 player_gravity = 0
+player_stand = pygame.image.load('graphics/player_walk/p2_walk03.png').convert_alpha() # Load the image and convert it to a surface
+player_stand = pygame.transform.rotozoom(player_stand, 90, 2)
+player_Stand_rect = player_stand.get_rect(center = (400, 200))
+
+game_name = font.render('Martian Runner', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400, 75))
+
+game_message = font.render('Press Space to Run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (390, 340))
 
 while True:
     for event in pygame.event.get():    # Get all events
@@ -63,7 +74,7 @@ while True:
         #pygame.draw.rect(screen, '#c0e8ec', score_rect)                            # Draw the surface on the screen
         #pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
         #screen.blit(text_surface, score_rect)                                   # Draw the surface on the screen
-        display_score()
+        score = display_score()
         snail_rect.x -= 4
         if snail_rect.right <= 0:
             snail_rect.left = screen_width
@@ -83,6 +94,13 @@ while True:
             game_active = False
     else:
         screen.fill((94,129,162))
+        screen.blit(player_stand, player_Stand_rect)
+
+        score_message = font.render(f'Your Score: {score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 325))
+        screen.blit(game_name, game_name_rect)
+        if score == 0: screen.blit(game_message, game_message_rect)
+        else: screen.blit(score_message, score_message_rect)
         
         
         # draw all elements
