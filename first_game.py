b04c42376ad256a4079251d30055f1c22905792f
Evan <116227653+itsevanb@users.noreply.github.com>
2023-04-02 17:21:35 -0500

    Update first_game.py
    learning concepts of pygame

--- first_game.py
+++ first_game.py
@@ -16,13 +16,14 @@ ground_surface = pygame.image.load('graphics/ground.png').convert() # Load the i
 ground_height = 100  # Adjust this value based on the desired height of the ground
 ground_surface = pygame.transform.scale(ground_surface, (screen_width, ground_height))
 
-text_surface = font.render('My Game', False, 'Red') # Create a surface with the text
+text_surface = font.render('My Game', False, (64,64,64)) # Create a surface with the text
+score_rect = text_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail.png').convert_alpha() # Load the image and convert it to a surface
 original_width, original_height = snail_surface.get_size()
 scaled_width, scaled_height = original_width * 2, original_height * 2
 snail_surface = pygame.transform.scale(snail_surface, (scaled_width, scaled_height))
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright = (600, screen_height - ground_height))
 
 player_surface = pygame.image.load('graphics/player_walk/p2_walk01.png').convert_alpha() # Load the image and convert it to a surface
 player_rect = player_surface.get_rect(midbottom = (80, screen_height - ground_height))
@@ -32,16 +33,37 @@ while True:
         if event.type == pygame.QUIT:   # If the event is quit
             pygame.quit()               # Quit pygame
             exit()                      # Quit python
+        #if event.type == pygame.MOUSEMOTION:
+            #if player_rect.collidepoint(event.pos): print('collision')
+        #if event.type == pygame.KEYDOWN:
+            #if event.key == pygame.K_SPACE:
+                #print('jump')
     
-    screen.blit(sky_surface,(0,0))      # Draw the surface on the screen
+    screen.blit(sky_surface,(0,0))                                           # Draw the surface on the screen
     screen.blit(ground_surface,(0, screen_height - ground_height))         # Draw the surface on the screen
-    screen.blit(text_surface,(300,50))         # Draw the surface on the screen
-    snail_x_pos -= 3
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos, screen_height - ground_height))         # Draw the surface on the screen
-    screen.blit(player_surface, player_rect)                 # Draw the surface on the screen
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)                            # Draw the surface on the screen
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    screen.blit(text_surface, score_rect)                                   # Draw the surface on the screen
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = screen_width
+    snail_y_pos = player_rect.bottom - scaled_height            # adjusts snail position to player position
+    screen.blit(snail_surface, snail_rect)                      # Draw the surface on the screen
+    screen.blit(player_surface, player_rect)                    # Draw the surface on the screen
 
+    #keys = pygame.key.get_pressed()
+    #keys[pygame.K_SPACE]:
+    #print('jump')
+
+    #if player_rect.colliderect(snail_rect):                     #Checks if the snail and player collide
+        #print("Collision")
+
+    """ mos_pos = pygame.mouse.get_pos()                            # Get the mouse position
+    if player_rect.collidepoint(mos_pos):
+        print(pygame.mouse.get_pressed())   """                     # Get the mouse button pressed
+    
+    
+    
     # draw all elements
     # update everything
     pygame.display.update() # Update the screen
