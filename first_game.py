99b6b5266ffbd11414367af0be2624cfced52f10
Evan <116227653+itsevanb@users.noreply.github.com>
2023-04-02 19:47:04 -0500

    bugs and updates
    finally got second enemy to appear along side first enemy
    updated the look of floor enemy

--- first_game.py
+++ first_game.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     current_time = int((pygame.time.get_ticks() / 1000)) - start_time
@@ -7,7 +8,17 @@ def display_score():
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf, score_rect)
     return current_time
-    
+
+def obstacle_move(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect, obstacle_surf in obstacle_list:
+            obstacle_rect.x -= 4
+            if obstacle_rect.right <= 0:
+                obstacle_list.remove((obstacle_rect, obstacle_surf))
+            screen.blit(obstacle_surf, obstacle_rect)
+        return obstacle_list
+    else:
+        return []    
 
 pygame.init()   # Initialize pygame
 screen_width = 800
@@ -30,11 +41,19 @@ ground_surface = pygame.transform.scale(ground_surface, (screen_width, ground_he
 #text_surface = font.render('My Game', False, (64,64,64)) # Create a surface with the text
 #score_rect = text_surface.get_rect(center = (400, 50))
 
-snail_surface = pygame.image.load('graphics/snail.png').convert_alpha() # Load the image and convert it to a surface
+snail_surface = pygame.image.load('graphics/slimeWalk1.png').convert_alpha() # Load the image and convert it to a surface
 original_width, original_height = snail_surface.get_size()
-scaled_width, scaled_height = original_width * 3, original_height * 3
+scaled_width, scaled_height = original_width * 1.25, original_height * 1.25
 snail_surface = pygame.transform.scale(snail_surface, (scaled_width, scaled_height))
 snail_rect = snail_surface.get_rect(bottomright = (700, screen_height - ground_height))
+
+fly_sur = pygame.image.load('graphics/fly.png').convert_alpha() # Load the image and convert it to a surface
+fly_original_width, fly_original_height = fly_sur.get_size()
+fly_scaled_width, fly_scaled_height = fly_original_width * 1.25, fly_original_height * 1.25
+fly_sur = pygame.transform.scale(fly_sur, (fly_scaled_width, fly_scaled_height))
+
+
+obstacle_rect_list = []
 
 player_surface = pygame.image.load('graphics/player_walk/p2_walk01.png').convert_alpha() # Load the image and convert it to a surface
 player_rect = player_surface.get_rect(midbottom = (80, screen_height - ground_height))
@@ -48,6 +67,10 @@ game_name_rect = game_name.get_rect(center = (400, 75))
 
 game_message = font.render('Press Space to Run', False, (111, 196, 169))
 game_message_rect = game_message.get_rect(center = (390, 340))
+
+#Timer
+o_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(o_timer, 1450)
 
 while True:
     for event in pygame.event.get():    # Get all events
@@ -67,7 +90,13 @@ while True:
                 game_active = True
                 snail_rect.left = 800
                 start_time = int(pygame.time.get_ticks() / 1000)
-    
+        if event.type == o_timer and game_active:
+            if randint(0, 2):
+                obstacle_rect_list.append((snail_surface.get_rect(bottomright=(randint(900, 1100), screen_height - ground_height)), snail_surface))
+            else:
+                obstacle_rect_list.append((fly_sur.get_rect(bottomright=(randint(900, 1100), 200)), fly_sur))
+
+
     if game_active:
         screen.blit(sky_surface,(0,0))                                           # Draw the surface on the screen
         screen.blit(ground_surface,(0, screen_height - ground_height))         # Draw the surface on the screen
@@ -75,11 +104,11 @@ while True:
         #pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
         #screen.blit(text_surface, score_rect)                                   # Draw the surface on the screen
         score = display_score()
-        snail_rect.x -= 4
-        if snail_rect.right <= 0:
-            snail_rect.left = screen_width
-        snail_y_pos = player_rect.bottom - scaled_height            # adjusts snail position to player position
-        screen.blit(snail_surface, snail_rect)                      # Draw the surface on the screen
+        #snail_rect.x -= 4
+        #if snail_rect.right <= 0:
+            #snail_rect.left = screen_width
+        #snail_y_pos = player_rect.bottom - scaled_height            # adjusts snail position to player position
+        #screen.blit(snail_surface, snail_rect)                      # Draw the surface on the screen
 
         #PLAYER MOVEMENT
         player_gravity += 1
@@ -88,6 +117,9 @@ while True:
             player_rect.bottom = screen_height - ground_height
             player_gravity = 0
         screen.blit(player_surface, player_rect)                    # Draw the surface on the screen
+
+        #OBSTACLE MOVEMENT
+        obstacle_rect_list = obstacle_move(obstacle_rect_list) # Move the obstacle
 
         #collision detection
         if snail_rect.colliderect(player_rect):
